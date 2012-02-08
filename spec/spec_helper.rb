d2a6146081bb7adbeb93f18f4d10daa2a4562a15
Jason Whittle <jason.whittle@gmail.com>
2012-02-08 14:19:26 -0500

    Set up RSpec and autospeccing.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'functional'
+
+require 'rspec/core'
+require 'rspec/expectations'
+require 'rr'
+
+begin
+  require 'ruby-debug'
+  $debugger = true
+rescue LoadError
+  $debugger = false
+end
+
+RSpec.configure do |config|
+  config.mock_with :rr
+  config.around(:each) { |ex| Debugger.start &ex }  if $debugger
+end
